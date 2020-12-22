aa07562339549f8174eea7d9c5c8a5bc345973cc
EC2 Default User <ec2-user@ip-172-31-35-137.ec2.internal>
2020-12-22 22:52:42 +0000

    added hero box - testing for mobile

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,20 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+import Hero from '../components/hero'
+
+
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    
+    
+    
+    <Hero/>
+    
+
+    
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something quick.</p>
