5f819f8eba050f901553beab0a1b05943aec841b
Andrew Kessinger <andrew.kessinger@gmail.com>
2022-03-09 16:09:04 +0100

    index

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,19 +18,19 @@ require("typeface-rubik")
 
 
 
-//hack to calculate the actual browser window height on mobile and save that value in the 'vh' variable which can be used later in CSS
-if (typeof window !== "undefined") {
-  // browser code
+// //hack to calculate the actual browser window height on mobile and save that value in the 'vh' variable which can be used later in CSS
+// if (typeof window !== "undefined") {
+//   // browser code
 
-  function appHeight() {
-      const doc = document.documentElement
-      doc.style.setProperty('--vh', (window.innerHeight*.01) + 'px');
-    }
-  
-    window.addEventListener('resize', appHeight);
-    appHeight();
+//   function appHeight() {
+//       const doc = document.documentElement
+//       doc.style.setProperty('--vh', (window.innerHeight*.01) + 'px');
+//     }
 
-}
+//     window.addEventListener('resize', appHeight);
+//     appHeight();
+
+// }
 
 
 export const query = graphql`
